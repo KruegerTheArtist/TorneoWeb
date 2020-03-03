40d6a18f75f31a3e049214b393a36bf3acaa3cf4
Крюгер <@->
2020-03-03 23:00:45 +0700

    new project/web

---
+++ torneo-web/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
