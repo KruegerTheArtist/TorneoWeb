ad1af918cdcb89065f1d48105d3d6dca49f38294
butenko <butenko@BUTENKO>
2021-03-24 14:10:16 +0700

    update angular to 11

--- torneo-web/src/app/material-block-module.ts
+++ torneo-web/src/app/material-block-module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatCardModule, MatDividerModule,
-  MatFormFieldModule, MatInputModule, MatDatepickerModule, MatTableModule, MatTooltipModule, MatSelectModule,
-  MatGridListModule, MatListModule, MatTabsModule, MatProgressSpinnerModule, MatPaginatorModule,
-  MatButtonToggleModule, MatTreeModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatMenuModule,
-  MatNativeDateModule, MatRadioModule, MatDialogModule, MatExpansionModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   imports: [
@@ -31,7 +51,6 @@ import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, Ma
     MatSliderModule,
     MatSlideToggleModule,
     MatMenuModule,
-    MatNativeDateModule,
     MatRadioModule,
     MatDialogModule,
     MatExpansionModule,
@@ -60,7 +79,6 @@ import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, Ma
     MatSliderModule,
     MatSlideToggleModule,
     MatMenuModule,
-    MatNativeDateModule,
     MatRadioModule,
     MatDialogModule,
     MatExpansionModule
