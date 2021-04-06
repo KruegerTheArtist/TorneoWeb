7ea10ca4eecd4f6a3b69552680b87f18cbc45da2
butenko <butenko@BUTENKO>
2021-04-06 22:32:08 +0700

    Добавил условия для hp и transmission

--- torneo-web/src/app/body/kit/kit.component.ts
+++ torneo-web/src/app/body/kit/kit.component.ts
@@ -25,6 +25,7 @@ export class KitComponent implements OnInit, OnDestroy {
   modification = this.fb.control(null);
   hp = this.fb.control(null);
   engine = this.fb.control(null);
+  customKit = this.fb.control(false);
 
 
   model = this.fb.group({
@@ -32,7 +33,8 @@ export class KitComponent implements OnInit, OnDestroy {
     engine: this.engine,
     transmission: this.transmission,
     modification: this.modification,
-    wheelDrive: this.wheelDrive
+    wheelDrive: this.wheelDrive,
+    customKit: this.customKit
   });
 
 
@@ -68,7 +70,7 @@ export class KitComponent implements OnInit, OnDestroy {
     if (this.modification.value === null) { this.modification.setValue(['Не выбрана модификация']); }
     if (this.hp.value === null) { this.hp.setValue(0); }
     console.log(this.model);
-  //  this.sub = this.model.valueChanges.subscribe(() => { this.initForm(); });
+    //  this.sub = this.model.valueChanges.subscribe(() => { this.initForm(); });
 
     // if (this.appState.getUserKit().includes('МКПП')) {
     //   this.configuration.transmission = 'МКПП';
@@ -136,70 +138,174 @@ export class KitComponent implements OnInit, OnDestroy {
     } else {
       this.model.get(control).setValue(kit);
     }
-    // if(!this.appState.getUserKit().includes(kit)) {
-    //   this.appState.setUserKit(kit);
-    // } else {}
   }
 
-  checkHp(modification: string) {
-    if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.VTE) {
-      return this.model.controls.hp.value === 140;
+  checkHp(hp: number) {
+    if (this.customKit.value)
+      return false;
+    let isF18b = EngineEnumExt.getEngineName(EngineEnum.f18b) === this.engine.value;
+    let isF20bSOHC = EngineEnumExt.getEngineName(EngineEnum.f20bSOHC) === this.engine.value;
+    let isF20bDOHC = EngineEnumExt.getEngineName(EngineEnum.f20bDOHC) === this.engine.value;
+    let isH22a = EngineEnumExt.getEngineName(EngineEnum.h22a) === this.engine.value;
+    let isH23a = EngineEnumExt.getEngineName(EngineEnum.h23a) === this.engine.value;
+    let isAT = TransmissionEnumExt.getTransmissionName(TransmissionEnum.AT) === this.transmission.value;
+    let isMT = TransmissionEnumExt.getTransmissionName(TransmissionEnum.MT) === this.transmission.value;
+    let isFrontWD = WheelDriveEnumExt.getWheelDriveName(WheelDriveEnum.frontWD) === this.wheelDrive.value;
+    let isFullWD = WheelDriveEnumExt.getWheelDriveName(WheelDriveEnum.fullWD) === this.wheelDrive.value;
+
+    switch (hp) {
+      case 140:
+        if (isF18b && isFrontWD) {
+          return false;
+        } else {
+          if (this.hp.value === hp) {
+            this.clear('hp');
+          }
+          return true;
+        }
+        break;
+      case 148:
+        if (isF20bSOHC && isAT && isFullWD) {
+          return false;
+        } else {
+          if (this.hp.value === hp) {
+            this.clear('hp');
+          }
+          return true;
+        }
+        break;
+      case 150:
+        if (isFrontWD && isF20bSOHC) {
+          return false;
+        } else {
+          if (this.hp.value === hp) {
+            this.clear('hp');
+          }
+          return true;
+        }
+        break;
+      case 180:
+        if (isFrontWD && isF20bDOHC && isAT) {
+          return false;
+        } else {
+          if (this.hp.value === hp) {
+            this.clear('hp');
+          }
+          return true;
+        }
+        break;
+      case 200:
+        if (isFrontWD && ((isF20bDOHC && isMT)) || (isH23a && isAT)) {
+          return false;
+        } else {
+          if (this.hp.value === hp)
+            this.clear('hp');
+          return true;
+        }
+        break;
+      case 220:
+        if (isFrontWD && isH22a && isMT) {
+          return false;
+        } else {
+          if (this.hp.value === hp) {
+            this.clear('hp');
+          }
+          return true;
+        }
+        break;
+      default:
+        break;
     }
-    if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.VTS) {
-      return this.model.controls.hp.value !== 140;
-    }
-    if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.euroR) {
-      return this.model.controls.hp.value === 220;
-    }
-    if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.sir) {
-      return this.model.controls.hp.value === 180
-      && this.model.controls.transmission.value === 'АКПП';
-    }
-    if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.sirT) {
-      return this.model.controls.hp.value === 200
-      && this.model.controls.transmission.value === 'МКПП';
-    }
+
   }
+
+
 
   checkTransmission(transmission: string) {
-    if (TransmissionEnumExt.getTransmissionEnum(transmission) !== TransmissionEnum.MT) {
-      return this.model.controls.hp.value === 220;
+    if (this.customKit.value)
+      return false;
+
+    let isF18b = EngineEnumExt.getEngineName(EngineEnum.f18b) === this.engine.value;
+    let isF20bSOHC = EngineEnumExt.getEngineName(EngineEnum.f20bSOHC) === this.engine.value;
+    let isH22a = EngineEnumExt.getEngineName(EngineEnum.h22a) === this.engine.value;
+    let isFullWD = WheelDriveEnumExt.getWheelDriveName(WheelDriveEnum.fullWD) === this.wheelDrive.value;
+
+    switch (transmission) {
+      case TransmissionEnumExt.getTransmissionName(TransmissionEnum.MT):
+        if (!isFullWD && (isH22a || isF18b || isF20bSOHC)) {
+          return false;
+        } else {
+          if (this.transmission.value === transmission) {
+            this.clear('transmission');
+          }
+          return true;
+        }
+        break;
+      case TransmissionEnumExt.getTransmissionName(TransmissionEnum.AT):
+        if (!isH22a) {
+          return false;
+        } else {
+          if (this.transmission.value === transmission) {
+            this.clear('transmission');
+          }
+          return true;
+        }
+        break;
     }
   }
 
   checkWheelDrive(wheelDrive: string) {
-    if (WheelDriveEnumExt.getWheelDriveEnum(wheelDrive) === WheelDriveEnum.fullWD) {
-      return this.model.controls.hp.value === 148;
+    if (this.customKit.value)
+      return false;
+
+    if (WheelDriveEnumExt.getWheelDriveEnum(wheelDrive) === WheelDriveEnum.rearWD) {
+      return !this.customKit.value;
     }
+    if (WheelDriveEnumExt.getWheelDriveEnum(wheelDrive) === WheelDriveEnum.fullWD) {
+      return this.hp.value !== 148 && EngineEnumExt.getEngineName(EngineEnum.f20bSOHC) !== this.engine.value
+    } else if (WheelDriveEnumExt.getWheelDriveEnum(wheelDrive) === WheelDriveEnum.frontWD) {
+      return this.hp.value === 148;
+    }
+
+    // if (WheelDriveEnumExt.getWheelDriveEnum(wheelDrive) === WheelDriveEnum.fullWD) {
+    //   return this.model.controls.hp.value === 148;
+    // }
   }
+
+  // checkWheelDrive(wheelDrive: string) {
+  //   if (WheelDriveEnumExt.getWheelDriveEnum(wheelDrive) === WheelDriveEnum.fullWD) {
+  //     return this.model.controls.hp.value === 148;
+  //   }
+  // }
 
   checkModification(modification: string) {
     if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.VTE) {
       return this.model.controls.engine.value === EngineEnumExt.getEngineName(EngineEnum.f18b)
-              && this.model.controls.hp.value === 140
-              && this.model.controls.wheelDrive.value === WheelDriveEnumExt.getWheelDriveName(WheelDriveEnum.frontWD);
+        && this.model.controls.hp.value === 140
+        && this.model.controls.wheelDrive.value === WheelDriveEnumExt.getWheelDriveName(WheelDriveEnum.frontWD);
     }
     if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.VTS) {
-      return this.model.controls.engine.value === EngineEnumExt.getEngineName(EngineEnum.f20b)
-              && this.model.controls.hp.value !== 140
-              && this.model.controls.hp.value !== 180
-              && this.model.controls.hp.value !== 200
-              && this.model.controls.hp.value !== 220;
+      return this.model.controls.engine.value === EngineEnumExt.getEngineName(EngineEnum.f20bSOHC)
+        && this.model.controls.hp.value !== 140
+        && this.model.controls.hp.value !== 180
+        && this.model.controls.hp.value !== 200
+        && this.model.controls.hp.value !== 220;
     }
     if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.euroR) {
       return this.model.controls.engine.value === EngineEnumExt.getEngineName(EngineEnum.h22a)
-              && this.model.controls.hp.value === 220;
+        && this.model.controls.hp.value === 220;
     }
     if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.sir) {
       return this.model.controls.hp.value === 180
-      && this.model.controls.transmission.value === 'АКПП';
+        && this.model.controls.transmission.value === 'АКПП';
     }
     if (ModificationEnumExt.getModificationEnum(modification) === ModificationEnum.sirT) {
       return this.model.controls.hp.value === 200
-      && this.model.controls.transmission.value === 'МКПП';
+        && this.model.controls.transmission.value === 'МКПП';
     }
     return false;
   }
+
 
   includeKit(kit: string) {
     // return this.appState.getUserKit().includes(kit);
@@ -213,5 +319,8 @@ export class KitComponent implements OnInit, OnDestroy {
     this.appState.setUserKit(this.model.value);
   }
 
+  private clear(controlName: string) {
+    this.model.controls[controlName].setValue(undefined);
+  }
 
 }
--- torneo-web/src/app/shared/enums/engine.enum.ts
+++ torneo-web/src/app/shared/enums/engine.enum.ts
@@ -1,7 +1,11 @@
+import { WheelDriveEnum } from "./wheel-drive.enum";
+
 export enum EngineEnum {
     f18b = 0,
-    f20b = 1,
-    h22a = 2
+    f20bSOHC = 1,
+    f20bDOHC = 2,
+    h22a = 3,
+    h23a = 4
 }
 
 export class EngineEnumExt {
@@ -9,29 +13,39 @@ export class EngineEnumExt {
         switch (engine) {
             case EngineEnum.f18b:
                 return 'f18b';
-            case EngineEnum.f20b:
-                return 'f20b';
+            case EngineEnum.f20bSOHC:
+                return 'f20bSOHC';
+            case EngineEnum.f20bDOHC:
+                return 'f20bDOHC';
             case EngineEnum.h22a:
                 return 'h22a';
+            case EngineEnum.h23a:
+                return 'h23a';
             default:
                 return 'Неизвестно';
         }
     }
 
     public static getAllEngineNames(): string[] {
-        return ['f18b', 'f20b', 'h22a'];
+        return ['f18b', 'f20bSOHC', 'f20bDOHC', 'h22a', 'h23a'];
     }
 
     public static getAllHpEngines(): number[] {
         return [140, 148, 150, 180, 200, 220];
     }
 
-    public static getHpByEngine(engine: EngineEnum): number {
+    public static getHpByEngine(engine: EngineEnum, whellDrive?: WheelDriveEnum): number {
         switch (engine) {
             case EngineEnum.f18b:
                 return 140;
-            case EngineEnum.f20b:
+            case (EngineEnum.f18b && whellDrive):
+                return 148;
+            case EngineEnum.f20bSOHC:
                 return 150;
+            case EngineEnum.f20bDOHC:
+                return 180;
+            case EngineEnum.h23a:
+                return 200;
             case EngineEnum.h22a:
                 return 220;
             default:
